62267b4b05f18da766720bd3215e97fa79c7a6bb
Dylan Kinzer <46228125+dkinzer5@users.noreply.github.com>
2021-01-07 14:35:32 -0800

    making changes

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,16 +1,17 @@
 <template>
   <div class="home">
-    <div class='postcard' v-for="post in posts" :key="post._id">
+    <h1>Hello</h1>
+    <!-- <div class='postcard' v-for="post in posts" :key="post._id">
       <h1>{{post.text}}</h1>
       <h1>{{post.createdBy}}</h1>
       <h1>{{post.createdAt}}</h1>
-    </div>
+    </div> -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import axios from 'axios';
+// import axios from 'axios';
 
 export default {
   name: 'Home',
@@ -22,12 +23,12 @@ export default {
       posts: [],
     };
   },
-  created() {
-    axios.get('http://localhost:5000/api/getAllPosts').then((res) => {
-      window.console.log(res);
-      this.posts = res.data;
-    });
-  },
+  // created() {
+  //   axios.get('http://localhost:5000/api/getAllPosts').then((res) => {
+  //     window.console.log(res);
+  //     this.posts = res.data;
+  //   });
+  // },
 };
 </script>
 
---
+++ server.js
@@ -0,0 +1,31 @@
+/* eslint-disable no-console */
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require("morgan");
+const history = require('connect-history-api-fallback');
+
+morgan("tiny")
+const app = express();
+
+
+// Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+const APIRoute = require('./server/routes/api');
+
+app.use('/api', APIRoute);
+app.use(history());
+app.use(express.static('./client/dist/'));
+app.get('/', (req, res) => {
+  res.sendFile('/client/dist/index.html');
+});
+
+const listenPort = process.env.PORT;
+const listenHost = process.env.HOST;
+app.listen(listenPort, () => {
+  console.log(`Server started on ${listenHost}:${listenPort}`);
+});

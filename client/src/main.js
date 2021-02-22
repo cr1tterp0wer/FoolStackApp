497116aa9f25e6d4a12b60d38314b2eb0508903f
cr1tterp0wer <capodacac@gmail.com>
2021-02-22 16:32:40 -0600

    task-134: heroku SOCKET_URL for vue_app Var

--- client/src/main.js
+++ client/src/main.js
@@ -10,7 +10,9 @@ import '../custom.scss';
 import 'bootswatch/dist/slate/bootstrap.min.css';
 import mixins from './helpers/common';
 
-const socketConnection = SocketIO('http://localhost:8999');
+const SOCKET_URL = VUE_APP_SOCKET_URL || 'http://localhost:8999';
+
+const socketConnection = SocketIO();
 
 Vue.use(new VueSocketIO({
   debug: true,

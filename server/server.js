1fc1873f0a2f882cd2838eedad9461bf6be533c0
cr1tterp0wer <capodacac@gmail.com>
2023-06-07 04:54:00 -0500

    main: cors

--- server/server.js
+++ server/server.js
@@ -8,16 +8,23 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const morgan = require("morgan");
 const history = require("connect-history-api-fallback");
+const socketIO = require("socket.io");
 
 const { HOST } = process.env;
+const { CLIENT_HOST } = process.env;
+const { VUE_PORT } = process.env;
 const { PORT } = process.env;
+
+const SERVER_URL = `${HOST}:${PORT}`;
+const CLIENT_URL = `${CLIENT_HOST}:${VUE_PORT}`;
+console.log(CLIENT_URL);
 
 morgan("tiny");
 
 // Middleware
 app.use(secure);
 app.use(bodyParser.json());
-app.use(cors({ credentials: true, origin: true }));
+app.use(cors({ credentials: true, origin: [CLIENT_URL] }));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 const APIRouter = require("./routes/api");
@@ -27,45 +34,32 @@ app.use((req, res, next) => {
 	next();
 });
 
-app.use("/api",
-	APIRouter);
+app.use("/api", APIRouter);
 app.use(history());
-app.use(express.static("../client/dist/"));
+//app.use(express.static("../client/dist/"));
 
-app.get("/",
-	(req, res) => {
-		res.sendFile("../client/dist/index.html");
-	});
+/*
+app.get("/", (req, res) => {
+	res.sendFile("../client/dist/index.html");
+});
+*/
 
-const server = app.listen(PORT,
-	() => {
-		console.log(`Server started on ${HOST}`);
-	});
-const socketIO = require("socket.io");
-const io = socketIO(server,
-	{
-		cors: {
-			allowedHeaders: [
-				"authorization",
-				"content-type"
-			],
-			credentials: true,
-			origin: HOST,
-			methods: [
-				"GET",
-				"POST"
-			],
-			transports: [
-				"websocket",
-				"polling"
-			],
-		},
-		allowEIO3: true,
-	});
+const server = app.listen(PORT, () => {
+	console.log(`Server started on ${SERVER_URL}`);
+});
 
-io.on("connection",
-	(socket) => {
-		console.log("Client connected");
-		socket.on("disconnect",
-			() => console.log("Client disconnected"));
-	});
+const io = socketIO(server, {
+	cors: {
+		allowedHeaders: ["authorization", "content-type"],
+		credentials: true,
+		origin: CLIENT_URL,
+		methods: ["GET", "POST"],
+		transports: ["websocket", "polling"],
+	},
+	allowEIO3: true,
+});
+
+io.on("connection", (socket) => {
+	console.log("Client connected");
+	socket.on("disconnect", () => console.log("Client disconnected"));
+});

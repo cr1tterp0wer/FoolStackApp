3bd89e708cdb9a4cbb763284d33810c79a4ec408
cr1tterp0wer <capodacac@gmail.com>
2023-06-24 17:40:53 -0500

    main: PageNotFound - link back to NU Social

--- client/src/views/PageNotFound.vue
+++ client/src/views/PageNotFound.vue
@@ -1,35 +1,35 @@
 <template>
-  <div id='nuNotFound' class='d-flex align-items-center justify-content-center'>
-    <div class=''>
+  <div id="nuNotFound" class="d-flex align-items-center justify-content-center">
+    <div class="">
       <h1>Oops.</h1>
       <h2>The page you're looking for doesn't exist.</h2>
-      <p>Go back to NU Social.</p>
+      <a href="/Home"><p>Go back to Home.</p></a>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'PageNotFound',
+  name: "PageNotFound",
 };
 </script>
 
 <style lang="scss">
-  .navbar {
-    display: none;
-  }
-  #content {
-    height:100vh;
-    width: 100%;
-    background-image: linear-gradient(#5371d3, #00d0ff);
-  }
-  #nuNotFound {
-    width: 100%;
-    height: 100%;
-    color: white;
+.navbar {
+  display: none;
+}
+#content {
+  height: 100vh;
+  width: 100%;
+  background-image: linear-gradient(#5371d3, #00d0ff);
+}
+#nuNotFound {
+  width: 100%;
+  height: 100%;
+  color: white;
 
-    h1 {
-      font-size: 20rem;
-    }
+  h1 {
+    font-size: 20rem;
   }
+}
 </style>

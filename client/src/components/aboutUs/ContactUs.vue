7d1831998f89ae057eb58c2efd31b87ce38212d0
cr1tterp0wer <capodacac@gmail.com>
2023-06-24 23:53:10 -0500

    main: AboutUs - href #about changed

--- client/src/components/aboutUs/ContactUs.vue
+++ client/src/components/aboutUs/ContactUs.vue
@@ -2,32 +2,28 @@
   <div>
     <!-- Contact Section -->
     <section
-      id='contact'
-      class='section-style'
-      data-background-image='@/assets/kite/images/background/contact.jpg'
+      id="contact"
+      class="section-style"
+      data-background-image="@/assets/kite/images/background/contact.jpg"
     >
-      <div class='pattern height-resize'>
-        <div class='container'>
-          <h3 class='section-name'>
-            <span>
-              Contact
-            </span>
+      <div class="pattern height-resize">
+        <div class="container">
+          <h3 class="section-name">
+            <span> Contact </span>
           </h3>
           <!-- /.section-name -->
-          <h2 class='section-title'>
-            Get in Touch
-          </h2>
-          <hr>
+          <h2 class="section-title">Get in Touch</h2>
+          <hr />
           <!-- /.Section-title  -->
-          <h3 class='section-description'>
-          Found a bug? Want to help? Questions comments and concerns can be sent directly to our
-          <a href='about-us/#about'>talented dev team</a> at
-          <a href="mailto:nusocial.contact@gmail.com">nusocial.contact@gmail.com</a>
+          <h3 class="section-description">
+            Found a bug? Want to help? Questions comments and concerns can be sent directly to our
+            <a href="#about">talented dev team</a> at
+            <a href="mailto:nusocial.contact@gmail.com">nusocial.contact@gmail.com</a>
           </h3>
           <!-- /.section-description -->
 
           <!-- /#contact-form -->
-          <div class='next-section'>
+          <div class="next-section">
             <a href="#content"><span></span></a>
           </div>
           <!-- /.next-section -->
@@ -45,17 +41,16 @@
 </template>
 
 <script>
-import Footer from '../Footer.vue';
+import Footer from "../Footer.vue";
 
 export default {
-  name: 'ContactUs',
+  name: "ContactUs",
   components: {
     Footer,
   },
 };
 </script>
 <style>
-
 #footer-section {
   background-color: white;
 }
